5bddc62f0ed9a078e09e1b6e4ea21946c6b6d9ae
Q <you@example.com>
2025-05-15 12:18:52 -0400

    Initialize subgraph

---
+++ graph/lens-prophet-lab2/tests/contract-utils.ts
@@ -0,0 +1,32 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, Bytes, BigInt } from "@graphprotocol/graph-ts"
+import { QuestionCreated } from "../generated/Contract/Contract"
+
+export function createQuestionCreatedEvent(
+  sender: Address,
+  postId: Bytes,
+  question: string,
+  amountSent: BigInt
+): QuestionCreated {
+  let questionCreatedEvent = changetype<QuestionCreated>(newMockEvent())
+
+  questionCreatedEvent.parameters = new Array()
+
+  questionCreatedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+  questionCreatedEvent.parameters.push(
+    new ethereum.EventParam("postId", ethereum.Value.fromFixedBytes(postId))
+  )
+  questionCreatedEvent.parameters.push(
+    new ethereum.EventParam("question", ethereum.Value.fromString(question))
+  )
+  questionCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "amountSent",
+      ethereum.Value.fromUnsignedBigInt(amountSent)
+    )
+  )
+
+  return questionCreatedEvent
+}
