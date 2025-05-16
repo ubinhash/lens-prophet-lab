b5448e05adbd521391e068719513a7fb234cc4b6
Q <you@example.com>
2025-05-16 16:15:26 -0400

    subgraph setup

--- graph/my-prophet-testnet-3/src/question-template-manager.ts
+++ graph/my-prophet-testnet-3/src/question-template-manager.ts
@@ -10,7 +10,7 @@ import {
   TemplateCreated,
   VariableAdded
 } from "../generated/schema"
-
+import { Bytes } from "@graphprotocol/graph-ts";
 export function handleQuestionCreated(event: QuestionCreatedEvent): void {
   let entity = new QuestionCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -25,24 +25,71 @@ export function handleQuestionCreated(event: QuestionCreatedEvent): void {
   entity.save()
 }
 
+// export function handleTemplateActivated(event: TemplateActivatedEvent): void {
+//   let entity = new TemplateActivated(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.QuestionTemplateManager_id = event.params.id
+//   entity.activated = event.params.activated
+
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+
+//   entity.save()
+// }
+
+
+
 export function handleTemplateActivated(event: TemplateActivatedEvent): void {
-  let entity = new TemplateActivated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.QuestionTemplateManager_id = event.params.id
-  entity.activated = event.params.activated
+  let id = Bytes.fromHexString(event.params.id.toHexString()) as Bytes;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let entity = TemplateActivated.load(id);
+  if (entity == null) {
+    entity = new TemplateActivated(id);
+  }
 
-  entity.save()
+  entity.QuestionTemplateManager_id = event.params.id;
+  entity.activated = event.params.activated;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+
+  let entity2 = TemplateCreated.load(id);
+  if (entity2 != null) {
+    entity2.activated = event.params.activated;
+    entity2.save();
+  }
 }
 
+
+// export function handleTemplateCreated(event: TemplateCreatedEvent): void {
+//   let entity = new TemplateCreated(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.QuestionTemplateManager_id = event.params.id
+//   entity.templateText = event.params.templateText
+//   entity.category = event.params.category
+
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+
+//   entity.save()
+// }
+
 export function handleTemplateCreated(event: TemplateCreatedEvent): void {
-  let entity = new TemplateCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let id = Bytes.fromHexString(event.params.id.toHexString()) as Bytes;
+
+  // Try loading existing entity or create new
+  let entity = TemplateCreated.load(id);
+  if (entity == null) {
+    entity = new TemplateCreated(id);
+    entity.activated = false;  // default to false on creation
+  }
+
   entity.QuestionTemplateManager_id = event.params.id
   entity.templateText = event.params.templateText
   entity.category = event.params.category
@@ -53,6 +100,7 @@ export function handleTemplateCreated(event: TemplateCreatedEvent): void {
 
   entity.save()
 }
+
 
 export function handleVariableAdded(event: VariableAddedEvent): void {
   let entity = new VariableAdded(
